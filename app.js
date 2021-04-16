c32984641d19561d659ea25327abfd79c77cf80f
richard savala <59234279+thrillhobaby@users.noreply.github.com>
2021-04-15 21:15:54 -0600

    Add files via upload
    in INDEX.HTML:
    put pause & play buttons inside btn container div
    
    in APP.JS:
    created variables to access:
    video-container 
    pause-btn
    play-btn
    preloader
    added event listener to pause-btn to pause 
    added event listener to play-btn to play
    added event listener to window to hide preloader after loading
    
    in STYLES.CSS:
    imported font Josefin Sans
    added variables for:
    primary font
    secondary font
    color 1
    color 2
    added initial styling to:
    body
    h1
    video-container
    preloader
    hide-preloader
    btn-container
    pause-btn
    play-btn
    header

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-// MDN
-// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
-// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+// video 
+const video = document.querySelector(".video-container");
 
-const btn = document.querySelector('.switch-btn');
-const video = document.querySelector('.video-container');
 
-btn.addEventListener('click', function() {
-    if (!btn.classList.contains("slide")) {
-        btn.classList.add("slide");
-        video.pause();
-    } else {
-        btn.classList.remove("slide");
-        video.play();
-    }
+// buttons
+const pauseBtn = document.querySelector(".pause-btn");
+const playBtn = document.querySelector(".play-btn");
+
+pauseBtn.addEventListener('click', function() {
+    video.pause();
 });
 
+playBtn.addEventListener('click', function() {
+    video.play();
+});
+
+
 // preloader
-const preloader = document.querySelector('.preloader');
+const preloader = document.querySelector(".preloader");
 
 window.addEventListener('load', function() {
     preloader.classList.add('hide-preloader');
